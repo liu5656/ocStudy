3940be7b8907591cac4165ce60d1bed6a2973909
刘健 <liujian_ios@163.com>
2017-01-17 16:04:32 +0800

    VideoToolBox硬件解码

---
+++ VideoToolBox_Encode/VideoToolBox_Encode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VideoToolBox_Encode
+//
+//  Created by 刘健 on 2017/1/16.
+//  Copyright © 2017年 刘健. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
