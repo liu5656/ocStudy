26140afcff70a25b5f9615c1b09dd733b417133a
刘健 <liujian_ios@163.com>
2017-02-15 08:49:45 +0800

    ffmpeg目录

---
+++ ffmpeg/ffmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ffmpeg
+//
+//  Created by 刘健 on 2017/2/10.
+//  Copyright © 2017年 刘健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
